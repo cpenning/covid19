337d80536acbf7517df4e6b2a03dc07645c8780d
Craig Pennington <cpenning@milo.org>
2020-04-05 17:48:23 +0000

    Allow comparisons with countries that have later first cases Format the output numbers

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Wed Apr  1 21:17:34 2020
 @description: Messing with the covid-19 data from the eu cdc
 """
 
+import locale
 import os
 import requests
 import sys
@@ -38,11 +39,11 @@ datafile_template = '{}/data/{}'
 fnmap = OrderedDict({
           'countryterritoryCode': ('Country', lambda x: x),
           'popData2018': ('Pop (2018)', lambda x: int(x+0.5)),
-          'Date' : ('Date', lambda x: x.date()),
+          'Date' : ('Comp. Date', lambda x: x.date()),
           'cases_cumulative': ('Cases', lambda x: int(x+0.5)),
           'deaths_cumulative': ('Deaths', lambda x: int(x+0.5)),
-          'deltaKC': ('ΔKOR (C)', lambda x: int(x+0.5)),
-          'deltaKP': ('ΔKOR (P)', lambda x: int(x+0.5))})
+          'deltaCC': ('ΔComp (C)', lambda x: int(x+0.5)),
+          'deltaCP': ('ΔComp (P)', lambda x: int(x+0.5))})
 
 def map_fields(d):
     """
@@ -71,9 +72,8 @@ def get_filename(fn):
             ofh.write(r.content)
     return cache_fn
 
-if __name__ == '__main__':
+def main(compisocc='KOR',today=date.today()):
     # Read today's news
-    today = date.today()
     dateRep = str(today)
     ecdc_fn=ecdc_fntemplate.format(dateRep)
     df = pd.read_excel(get_filename(ecdc_fn))
@@ -103,41 +103,50 @@ if __name__ == '__main__':
         idx = country_data.cases_cumulative.ne(0).idxmax()
         first_cases[country] = country_data.loc[idx]['Date'].date()
 
-    # Get Korea's Numbers
-    rok = country_groups.get_group("KOR")   
+    # Get Comparison Numbers
+    comp = country_groups.get_group(compisocc)
 
-    korday0 = first_cases['KOR']
+    compday0 = first_cases[compisocc]
 
-    # Keep track of where in Korea's timeline a country is
-    # XXX: will break things for any country that had a case before KOR
-    kor=dict()
-    kordays = today - korday0
-    
-    for k in first_cases:
-        cdays = today-first_cases[k]
-        kday = korday0 + cdays
-        kor[k] = rok[rok['Date'] ==  datetime.combine(kday, time.min)].to_dict()
+    # Keep track of where in the comparison timeline a country is
+    compdict=dict()
+    countryday = dict()
 
-    # Now we get to our comparisons.
-    # TODO: Figure out an ordering that makes sense
+    compdays = today - compday0
+
     countries = ['AUS', 'TWN', 'KOR', 'DEU', 'USA', 'IRN', 'ESP', 'ITA']
-
     cdata = OrderedDict()
 
-
+    dttoday = datetime.combine(today, time.min)
     for country in countries:
+        cdays = today-first_cases[country]
+        if compdays >= cdays:
+            # Comparison country has had the infection as long or longer
+            #   than this country. Look at where the comparison country was
+            #   relative to this country
+            cday = compday0 + cdays
+            compdict[country] = comp[comp['Date'] ==  datetime.combine(cday, time.min)].to_dict()
+            countryday[country] = dttoday
+        else:
+            # Comparison country has not had the infection as long as this
+            #   country. Look at where this country was relative to the
+            #   comparison country
+            cday = dttoday - (compday0 - first_cases[country])
+            compdict[country] = comp[comp['Date'] == dttoday].to_dict()
+            countryday[country] = cday
+
         c = country_groups.get_group(country).copy()
-        
-        # Get the relative point in time where we are for KOR
-        kdc = kor[country]['dc'][list(kor[country]['dc'])[0]]
-        kdp = kor[country]['dp'][list(kor[country]['dp'])[0]]
+
+        # Get the relative point in time where we are for the comparison
+        cdc = compdict[country]['dc'][list(compdict[country]['dc'])[0]]
+        cdp = compdict[country]['dp'][list(compdict[country]['dp'])[0]]
 
         # HERE! Here is the comparison code.
-        c['deltaKC'] = c['cases_cumulative']*(c['dc'] - kdc)
-        c['deltaKP'] = c['deaths_cumulative']*(c['dp'] - kdp)/c['dp']
+        c['deltaCC'] = c['cases_cumulative']*(c['dc'] - cdc)
+        c['deltaCP'] = c['deaths_cumulative']*(c['dp'] - cdp)/c['dp']
 
-        # Get today's data for this country
-        cdata[country] = c[c['dateRep'] == dateRep].to_dict()
+        # Get data for this country
+        cdata[country] = c[c['Date'] == countryday[country]].to_dict()
 
         # Good God, there's got to be a better way
         for k in cdata[country]:
@@ -152,17 +161,34 @@ if __name__ == '__main__':
 
     ctable = pd.DataFrame(cdata).T
     print(ctable)
+    
+    vtable = list()
+    
+    for row in ctable.values:
+        vrow = list()
+        for cell in row:
+            if isinstance(cell, int):
+                vstr = locale.format_string("%d", cell, grouping=True)
+                vrow.append(vstr)
+            else:
+                vrow.append(cell)
+        vtable.append(vrow)
 
     # Display the table
-    columns = tuple(countries)
     fig, ax = plt.subplots()
 
     # hide axes
     fig.patch.set_visible(False)
+    sttemplate = 'Comparison of COVID-19 fatalities relative to {}'
+    plt.title(sttemplate.format(compisocc),pad=0)
     ax.axis('off')
     ax.axis('tight')
-    t = ax.table(cellText=ctable.values, colLabels=ctable.columns, loc='center')
+    t = ax.table(cellText=vtable, colLabels=ctable.columns, loc='center')
     t.auto_set_font_size(False)
     t.set_fontsize(8)
     fig.tight_layout()
     plt.show()
+
+if __name__ == '__main__':
+    # TODO: Maybe add command line arguments
+    main()
